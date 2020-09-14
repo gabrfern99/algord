780736021909bd84984593ae5bf5cc5c3611bc03
Marcos <marcosvaleriox12@hotmail.com>
2020-09-14 13:29:14 -0300

    Remove comments and tests from sortMethos.py

--- sortMethods.py
+++ sortMethods.py
@@ -1,9 +1,3 @@
-# To do:
-# insertionSort()
-# selection
-# merge
-# quick
-
 import sys
 
 def insertion_sort(arr):
@@ -77,11 +71,3 @@ def quick_sort(arr, low, high):
 
 		quick_sort(arr, low, pivot_idx - 1)
 		quick_sort(arr, pivot_idx + 1, high)
-	
-
-#### MAIN ####
-
-arr = [ 5, 4, 3, 2, 1 ]
-quick_sort(arr, 0, len(arr) - 1)
-for i in range(len(arr)):
-	print(arr[i])
