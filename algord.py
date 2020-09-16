02d43f8c0d651160a34309f0e34d3c4b8f80d82f
Marcos <marcosvaleriox12@hotmail.com>
2020-09-16 12:00:09 -0300

    Make the functions initialize the number of comparisons and movements with 0

--- algord.py
+++ algord.py
@@ -8,13 +8,13 @@ def main():
     retlist, method = interface.csv_parser()
     sys.setrecursionlimit(len(retlist))
     if(method == "1"):
-        comps, moves = sortMethods.insertion_sort(retlist)
+        comps, moves = sortMethods.insertion_sort(retlist, 0, 0)
     elif(method == "2"):
-        comps, moves = sortMethods.selection_sort(retlist)
+        comps, moves = sortMethods.selection_sort(retlist, 0, 0)
     elif(method == "3"):
-        comps, moves = sortMethods.merge_sort(retlist)
+        comps, moves = sortMethods.merge_sort(retlist, 0, 0)
     elif(method == "4"):
-        comps, moves = sortMethods.quick_sort(retlist, 0, len(retlist) - 1)
+        comps, moves = sortMethods.quick_sort(retlist, 0, len(retlist) - 1, 0, 0)
     for i in range(len(retlist)):
         print(retlist[i])
     print("Comparisons: %d\nMovements: %d\n" % (comps, moves))
