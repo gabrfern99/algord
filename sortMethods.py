49ba2c4e9f0a08081a013903b02cd83f859f2473
Marcos <marcosvaleriox12@hotmail.com>
2020-09-16 15:36:09 -0300

    Add comparisons and movements counters on the functions

--- sortMethods.py
+++ sortMethods.py
@@ -1,32 +1,37 @@
-def insertion_sort(arr):
+def insertion_sort(arr, comps, moves):
     for j in range(1, len(arr)):
-
         temp = arr[j]
         i = j - 1
-
         while i >= 0 and temp < arr[i]:
             arr[i + 1] = arr[i]
             i -= 1
-            arr[i + 1] = temp
+            comps += 1
+            moves += 1
+        comps += 1
+        arr[i + 1] = temp
 
+    return comps, moves
 
-def selection_sort(arr):
+def selection_sort(arr, comps, moves):
     for i in range(len(arr)):
         min_idx = i
         for j in range(i + 1, len(arr)):
             if arr[min_idx] > arr[j]:
+                comps += 1
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        moves += 2
+    return comps, moves
 
-def merge_sort(arr):
+def merge_sort(arr, comps, moves):
     if len(arr) > 1:
         middle = len(arr) // 2
 
         left = arr[:middle]
         right = arr[middle:]
 
-        merge_sort(left)
-        merge_sort(right)
+        merge_sort(left, comps, moves)
+        merge_sort(right, comps, moves)
 
         i = j = k = 0
 
@@ -34,10 +39,14 @@ def merge_sort(arr):
             if left[i] <= right[j]:
                 arr[k] = left[i]
                 i += 1
+                comps += 1
+                moves += 1
             else:
                 arr[k] = right[j]
                 j += 1
                 k += 1
+                comps += 1
+                moves += 1
 # It may happen the program to fill completely the first vector while the
 # second is not completely filled yet, i.e, all elements in the left vector
 # are bigger than the elements in the second.
@@ -47,12 +56,15 @@ def merge_sort(arr):
             arr[k] = left[i]
             i += 1
             k += 1
+            moves += 1
         while j < len(right):
             arr[k] = right[j]
             j += 1
             k += 1
+            moves += 1
+    return comps, moves
 
-def partition(arr, low, high):
+def partition(arr, low, high, comps, moves):
     pivot = arr[high]
     i = low - 1
 
@@ -60,13 +72,16 @@ def partition(arr, low, high):
         if arr[j] < pivot:
             i += 1
             arr[j], arr[i] = arr[i], arr[j]
+            comps += 1
+            moves += 2
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
+    moves += 2
+    return i + 1, comps, moves
 
-def quick_sort(arr, low, high):
+def quick_sort(arr, low, high, comps, moves):
     if low < high:
-        pivot_idx = partition(arr, low, high)
+        pivot_idx, comps, moves = partition(arr, low, high, comps, moves)
         
-        quick_sort(arr, low, pivot_idx - 1)
-        quick_sort(arr, pivot_idx + 1, high)
-
+        quick_sort(arr, low, pivot_idx - 1, comps, moves)
+        quick_sort(arr, pivot_idx + 1, high, comps, moves)
+    return comps, moves
