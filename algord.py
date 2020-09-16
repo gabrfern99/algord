3d8092ca2ca2c3e68c6f47b734ebfada8550ad67
Gabriel Fernando <gabr.fern99@gmail.com>
2020-09-16 20:19:13 -0300

    Change time calculation methodd

--- algord.py
+++ algord.py
@@ -8,25 +8,23 @@ def main():
     retlist, method = interface.csv_parser()
     sys.setrecursionlimit(len(retlist))
     if(method == "1"):
-        start = time.time()
+        start = time.process_time()
         comps, moves = sortMethods.insertion_sort(retlist, 0, 0)
-        end = time.time()
     elif(method == "2"):
-        start = time.time()
+        start = time.process_time()
         comps, moves = sortMethods.selection_sort(retlist, 0, 0)
-        end =  time.time()
     elif(method == "3"):
-        start = time.time()
+        start = time.process_time()
         comps, moves = sortMethods.merge_sort(retlist, 0, 0)
-        end = time.time()
     elif(method == "4"):
-        start = time.time()
+        start = time.process_time()
         comps, moves = sortMethods.quick_sort(retlist, 0, len(retlist) - 1, 0, 0)
-        end = time.time()
     for i in range(len(retlist)):
-        print(retlist[i])
+        print(retlist[i], time.process_time() - start, sep=" - Current time: ")
+    print("\nTime elapsed:", time.process_time() - start)
     print("Comparisons: %d\nMovements: %d\n" % (comps, moves))
-    print("\nTime elapsed:", end - start)
+
+
 
 if __name__ == "__main__":
     main()
