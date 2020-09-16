cf2f08c1487fb11ac78870937559bb8355d25b85
Marcos <marcosvaleriox12@hotmail.com>
2020-09-16 11:39:53 -0300

    Add a counter for comparisons and movements

--- algord.py
+++ algord.py
@@ -8,15 +8,16 @@ def main():
     retlist, method = interface.csv_parser()
     sys.setrecursionlimit(len(retlist))
     if(method == "1"):
-        sortMethods.insertion_sort(retlist)
+        comps, moves = sortMethods.insertion_sort(retlist)
     elif(method == "2"):
-        sortMethods.selection_sort(retlist)
+        comps, moves = sortMethods.selection_sort(retlist)
     elif(method == "3"):
-        sortMethods.merge_sort(retlist)
+        comps, moves = sortMethods.merge_sort(retlist)
     elif(method == "4"):
-        sortMethods.quick_sort(retlist, 0, len(retlist) - 1)
+        comps, moves = sortMethods.quick_sort(retlist, 0, len(retlist) - 1)
     for i in range(len(retlist)):
         print(retlist[i])
+    print("Comparisons: %d\nMovements: %d\n" % (comps, moves))
 
 if __name__ == "__main__":
     main()
