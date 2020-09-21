be4a011519df620cd1364c7cf4859ba55ff5dcb1
Marcos <marcosvaleriox12@hotmail.com>
2020-09-21 17:22:24 -0300

    Come back when the things worked

--- sortMethods.py
+++ sortMethods.py
@@ -1,35 +1,24 @@
-comps = 0
-moves = 0
 def insertion_sort(arr):
-    global comps, moves
     for j in range(1, len(arr)):
+
         temp = arr[j]
         i = j - 1
-        comps += 1
+
         while i >= 0 and temp < arr[i]:
             arr[i + 1] = arr[i]
             i -= 1
-            moves += 1
-        arr[i + 1] = temp
+            arr[i + 1] = temp
 
-    return comps, moves
 
 def selection_sort(arr):
-    global comps, moves
     for i in range(len(arr)):
         min_idx = i
         for j in range(i + 1, len(arr)):
-            comps += 1
             if arr[min_idx] > arr[j]:
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        if arr[i] != arr[min_idx]:
-            moves += 1
-
-    return comps, moves
 
 def merge_sort(arr):
-    global comps, moves
     if len(arr) > 1:
         middle = len(arr) // 2
 
@@ -42,8 +31,6 @@ def merge_sort(arr):
         i = j = k = 0
 
         while i < len(left) and j < len(right):
-            comps += 1
-            moves += 1
             if left[i] <= right[j]:
                 arr[k] = left[i]
                 i += 1
@@ -51,19 +38,19 @@ def merge_sort(arr):
                 arr[k] = right[j]
                 j += 1
                 k += 1
+# It may happen the program to fill completely the first vector while the
+# second is not completely filled yet, i.e, all elements in the left vector
+# are bigger than the elements in the second.
+# To prevent the loss of elements, we do this:
 
         while i < len(left):
             arr[k] = left[i]
-            moves += 1
             i += 1
             k += 1
         while j < len(right):
             arr[k] = right[j]
-            moves += 1
             j += 1
             k += 1
-
-    return comps, moves
 
 def partition(arr, low, high):
     pivot = arr[high]
@@ -73,11 +60,8 @@ def partition(arr, low, high):
         if arr[j] < pivot:
             i += 1
             arr[j], arr[i] = arr[i], arr[j]
-            comps += 1
-            moves += 2
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    moves += 2
-    return i + 1, comps, moves
+    return i + 1
 
 def quick_sort(arr, low, high):
     if low < high:
@@ -85,4 +69,4 @@ def quick_sort(arr, low, high):
         
         quick_sort(arr, low, pivot_idx - 1)
         quick_sort(arr, pivot_idx + 1, high)
-    return comps, moves
+
