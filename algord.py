070f8d709b68a85691448615ae5d59ced1bf379d
Marcos <marcosvaleriox12@hotmail.com>
2020-09-21 17:51:22 -0300

    I don't know how to use git, i'm a shit, ignore the past commits

--- algord.py
+++ algord.py
@@ -6,22 +6,26 @@ import time
 
 def main():
     retlist, method = interface.csv_parser()
-    sys.setrecursionlimit(len(retlist))
+    sys.setrecursionlimit(len(retlist) + 4)
     if(method == "1"):
         start = time.process_time()
-        sortMethods.insertion_sort(retlist)
+        comps, moves = sortMethods.insertion_sort(retlist)
     elif(method == "2"):
         start = time.process_time()
-        sortMethods.selection_sort(retlist)
+        comps, moves = sortMethods.selection_sort(retlist)
     elif(method == "3"):
         start = time.process_time()
-        sortMethods.merge_sort(retlist)
+        comps, moves = sortMethods.merge_sort(retlist)
     elif(method == "4"):
         start = time.process_time()
-        sortMethods.quick_sort(retlist, 0, len(retlist) - 1)
+        comps, moves = sortMethods.quick_sort(retlist, 0, len(retlist) - 1)
     for i in range(len(retlist)):
         print(retlist[i], time.process_time() - start, sep=" - Current time: ")
     print("\nTime elapsed:", time.process_time() - start)
+    print("Comparisons: %d\nMovements: %d\n" % (comps, moves))
+
+
 
 if __name__ == "__main__":
     main()
+    exit(1)
--- sortMethods.py
+++ sortMethods.py
@@ -1,21 +1,35 @@
+comps = 0
+moves = 0
 def insertion_sort(arr):
+    global comps, moves
     for j in range(1, len(arr)):
         temp = arr[j]
         i = j - 1
+        comps += 1
         while i >= 0 and temp < arr[i]:
             arr[i + 1] = arr[i]
             i -= 1
-            arr[i + 1] = temp
+            moves += 1
+        arr[i + 1] = temp
+
+    return comps, moves
 
 def selection_sort(arr):
+    global comps, moves
     for i in range(len(arr)):
         min_idx = i
         for j in range(i + 1, len(arr)):
+            comps += 1
             if arr[min_idx] > arr[j]:
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        if arr[i] != arr[min_idx]:
+            moves += 1
+
+    return comps, moves
 
 def merge_sort(arr):
+    global comps, moves
     if len(arr) > 1:
         middle = len(arr) // 2
 
@@ -28,6 +42,8 @@ def merge_sort(arr):
         i = j = k = 0
 
         while i < len(left) and j < len(right):
+            comps += 1
+            moves += 1
             if left[i] <= right[j]:
                 arr[k] = left[i]
                 i += 1
@@ -38,27 +54,37 @@ def merge_sort(arr):
 
         while i < len(left):
             arr[k] = left[i]
+            moves += 1
             i += 1
             k += 1
         while j < len(right):
             arr[k] = right[j]
+            moves += 1
             j += 1
             k += 1
 
+    return comps, moves
+
 def partition(arr, low, high):
+    global comps, moves
     pivot = arr[high]
     i = low - 1
 
     for j in range(low, high):
+        comps += 1
         if arr[j] < pivot:
             i += 1
             arr[j], arr[i] = arr[i], arr[j]
+            moves += 1
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
+    moves += 1
+    return i + 1, comps, moves
 
 def quick_sort(arr, low, high):
+    global comps, moves
     if low < high:
-        pivot_idx = partition(arr, low, high)
+        pivot_idx, comps, moves = partition(arr, low, high)
         
         quick_sort(arr, low, pivot_idx - 1)
         quick_sort(arr, pivot_idx + 1, high)
+    return comps, moves
